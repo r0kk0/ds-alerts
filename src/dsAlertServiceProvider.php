4e88bb6bc57c5f5a204391549798724c4328f6de
Daniele <r0kk0z@gmail.com>
2016-03-10 22:45:26 +0100

    cambiato nome package

--- src/dsAlertServiceProvider.php
+++ src/dsAlertServiceProvider.php
@@ -35,8 +35,8 @@ class dsAlertServiceProvider extends ServiceProvider
     protected function publishFiles()
     {
         $this->publishes([
-            __DIR__ . '/librerie/sweetalert.css' => public_path('css/sweetalert.css'),
-            __DIR__ . '/librerie/sweetalert.js' => public_path('js/sweetalert.js'),
+            __DIR__ . '/librerie/sweetalert.css' => base_path('resources/assets/css/sweetalert.css'),
+            __DIR__ . '/librerie/sweetalert.js' => base_path('resources/assets/js/sweetalert.js'),
             __DIR__ . '/views/dsalerts.blade.php' => base_path('resources/views/vendor/dsalerts/dsalerts.blade.php'),
         ], 'dsalerts');
     }
